70fad1201aa4ed6657db18ea9103ca9106affdad
qngdt <quangdt.dev@gmail.com>
2023-09-17 22:30:39 +0700

    1st commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# What is this?
+---
+- This repo is used to extract words from [TTP's GRE Vocabulary PDF](https://gre.blog.targettestprep.com/gre-vocabulary/) using OpenAI GPT-3.5 API and Langchain
+
+## Requirements
+- An `.env` file containing `OPENAI_API_KEY`
+
+## Contents
+- `main.py`: Extracting words from each page of the PDF and saving them to `output/data-{page-number}.json` file
+- `anki.py`: Import the data from `output` folder to Anki
---
+++ main.py
@@ -0,0 +1,36 @@
+from io import StringIO
+import json
+from dotenv import load_dotenv
+from pdfminer.high_level import extract_text
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import create_extraction_chain
+
+
+output_string = StringIO()
+
+load_dotenv()
+
+PAGES = 70
+
+for page in range(1, PAGES + 1):
+    print("Extracting page", page)
+    text = extract_text("gre_vocab.pdf", page_numbers=[page])
+
+    schema = {
+        "properties": {
+            "number": {"type": "integer"},
+            "word": {"type": "string"},
+            "word_type": {"type": "string"},
+            "meaning": {"type": "string"},
+        },
+    }
+
+    llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
+    chain = create_extraction_chain(schema, llm)
+    res = chain.run(text)
+    output_path = f"output/data-{page}.json"
+    with open(output_path, "w") as output_file:
+        output_file.write(json.dumps(res))
+
+    print("Wrote to", output_path)
+    print("--------------------")
